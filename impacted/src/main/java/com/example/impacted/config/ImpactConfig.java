34bd92d3d8a5b2f07de1d43a62032c10f69cac2e
n-e-s-i-r-x <rakutadrakutan@gmail.com>
2026-04-22 04:19:15 +0800

    Add files via upload

---
+++ impacted/src/main/java/com/example/impacted/config/ImpactConfig.java
@@ -0,0 +1,9 @@
+
+package com.example.impacted.config;
+
+public class ImpactConfig {
+    public static boolean enabled = true;
+    public static float intensity = 1.0f;
+    public static int cooldown = 10;
+    public static boolean editMode = true;
+}
---
+++ impacted/src/main/java/com/example/impacted/config/ModMenuEntry.java
@@ -0,0 +1,8 @@
+
+package com.example.impacted.config;
+
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenuEntry implements ModMenuApi {
+    // placeholder (real Cloth Config screen can be added later)
+}
